d60a8046b2a70164397f33c7cf8d901b479bd838
XuanLin123 <f131406298@gmail.com>
2022-11-25 16:12:22 +0800

    改到一半

--- app1/app/src/main/java/com/example/kinmenfoodmap/accFragment.java
+++ app1/app/src/main/java/com/example/kinmenfoodmap/accFragment.java
@@ -3,6 +3,8 @@ package com.example.kinmenfoodmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,42 +13,13 @@ import android.widget.Button;
 import android.widget.TextView;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link accFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class accFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public accFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment accFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static accFragment newInstance(String param1, String param2) {
-        accFragment fragment = new accFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -71,46 +44,25 @@ public class accFragment extends Fragment {
         Button btn4 = (Button) view.findViewById(R.id.btn_background);
         Button btn5 = (Button) view.findViewById(R.id.btn_scoreapp);
         Button btn6 = (Button) view.findViewById(R.id.btn_aboutus);
-
         System.out.println("還是這裡嗎");
 
         btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                view = inflater.inflate(R.layout.fragment_surprise, container, false);
-            }
-        });
-        btn2.setOnClickListener(new View.OnClickListener(){
-             @Override
-             public void onClick(View view) {
-                 view = inflater.inflate(R.layout.fragment_surprise, container, false);
-             }
-        });
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                view = inflater.inflate(R.layout.fragment_surprise, container, false);
-            }
-        });
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                view = inflater.inflate(R.layout.fragment_surprise, container, false);
-            }
-        });
-        btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                view = inflater.inflate(R.layout.fragment_surprise, container, false);
-            }
-        });
-        btn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                view = inflater.inflate(R.layout.fragment_surprise, container, false);
-            }
-        });
+                                    @Override
+                                    public void onClick(View view) {
+                                        System.out.println("AAAA");
+                                       // replaceFragment(new accFragment());
+                                      //  view = inflater.inflate(R.layout.acc_record, container, false);
+                                    }
+                                });
+
     return view;
 
+    }
+
+    private  void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.menu_layout,fragment);
+        fragmentTransaction.commit();
     }
 }

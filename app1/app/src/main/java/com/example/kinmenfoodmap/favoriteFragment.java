783d0c633784b6b2f0bf46b570f45cb96e714bd0
Kim Lin <linkim0914@gmail.com>
2022-11-29 23:08:34 +0800

    資料庫 查看、修改OK

--- app1/app/src/main/java/com/example/kinmenfoodmap/favoriteFragment.java
+++ app1/app/src/main/java/com/example/kinmenfoodmap/favoriteFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -12,6 +14,14 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class favoriteFragment extends Fragment implements View.OnClickListener {
 
@@ -38,6 +48,63 @@ public class favoriteFragment extends Fragment implements View.OnClickListener {
             @Override
             public void onClick(View view) {
                 output.setText("Button 1");
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("FirstClass");
+                query.whereEqualTo("objectId", "0mfC6T5HHR");
+
+
+                System.out.println(query);
+                query.getFirstInBackground(new GetCallback<ParseObject>() {
+                    public void done(ParseObject player, ParseException e) {
+                        System.out.println("10");
+                        if (e == null) {
+                            System.out.println("ok:");
+                            System.out.println(player.getString("message"));
+                        } else {
+                            System.out.println("error");
+                            // Something is wrong
+                        }
+                    }
+                });
+
+                query.getInBackground("0mfC6T5HHR", new GetCallback<ParseObject>() {
+                    public void done(ParseObject player, ParseException e) {
+                        if (e == null) {
+                            // Now let's update it with some new data. In this case, only cheatMode and score
+                            // will get sent to the Parse Cloud. playerName hasn't changed.
+                            player.put("yearOfBirth", 1998);
+                            player.put("emailContact", "a.wed@domain.io");
+                            player.put("message","yes");
+                            player.saveInBackground();
+                        } else {
+                            // Failed
+                        }
+                    }
+                });
+
+                /*
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if (e == null) {
+                            objectsWereRetrievedSuccessfully(objects);
+                            System.out.println("1");
+                            System.out.println(objects);
+                        } else {
+                            System.out.println(objects);
+                            System.out.println("2");
+                            output.setText("db error");
+                        }
+                    }
+
+
+                    private void objectsWereRetrievedSuccessfully(List<ParseObject> objects) {
+                        System.out.println("3");
+                        System.out.println(objects);
+                        output.setText(objects.toString());
+                    }
+                }); */
+
+
+
             }
         });
         btn2.setOnClickListener(new View.OnClickListener(){
@@ -45,6 +112,18 @@ public class favoriteFragment extends Fragment implements View.OnClickListener {
             @Override
             public void onClick(View view) {
                 output.setText("Button 2");
+
+//      vvvvvvvvvvvvvvvvvvvvvv  example: add something to remoted database   vvvvvvvvvvvvvvvvvvvvvvvvvvvv
+                ParseObject firstObject = new ParseObject("FirstClass");
+                firstObject.put("message","Hey ! First message from android. Parse is now connected");
+                firstObject.saveInBackground(e -> {
+                    if (e != null){
+                        Log.e("MainActivity", e.getLocalizedMessage());
+                    }else{
+                        Log.d("MainActivity","Object saved.");
+                    }
+                });
+//      ^^^^^^^^^^^^^^^^^^^^^^  example: add something to remoted database   ^^^^^^^^^^^^^^^^^^^^^^^^^^^
             }
         });
 

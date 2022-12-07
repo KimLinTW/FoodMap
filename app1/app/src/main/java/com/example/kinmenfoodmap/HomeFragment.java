d39bd9a5da1d66bf5fbb4c240aaaac9151d64559
吳柏翰 <eggwu@eggwuMacBook.local>
2022-12-07 16:23:27 +0800

    threadProblem

--- app1/app/src/main/java/com/example/kinmenfoodmap/HomeFragment.java
+++ app1/app/src/main/java/com/example/kinmenfoodmap/HomeFragment.java
@@ -1,85 +1,249 @@
 package com.example.kinmenfoodmap;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.kinmenfoodmap.databinding.ActivityMainBinding;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
 public class HomeFragment extends Fragment {
+   ActivityMainBinding binding;
+   ArrayList<String> userlist;
+    ArrayAdapter<String> listAdapter;
+    Handler handler = new Handler();
+    ProgressDialog progressDialog;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-//      vvvvvvvvvvvvvvvvvvvvvv  example: add something to remoted database   vvvvvvvvvvvvvvvvvvvvvvvvvvvv
-        ParseObject firstObject = new ParseObject("FirstClass");
-        firstObject.put("message","Hey ! First message from android. Parse is now connected");
-        firstObject.saveInBackground(e -> {
-            if (e != null){
-                Log.e("MainActivity", e.getLocalizedMessage());
-            }else{
-                Log.d("MainActivity","Object saved.");
-            }
-        });
-//      ^^^^^^^^^^^^^^^^^^^^^^  example: add something to remoted database   ^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button btn1 = (Button) view.findViewById(R.id.button);
+        ListView list1 = (ListView)view.findViewById(R.id.userlist);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
+        //items = getActivity().getResources().getString(R.id.userlist);
+        userlist = new ArrayList<>();
+        listAdapter = new ArrayAdapter<String>(view.getContext(),
+                android.R.layout.simple_list_item_1,userlist);
+                list1.setAdapter(listAdapter);
+    //  initialUserlist();
+       new fetchData().start();
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                new fetchData().start();
+
+            }
+        });
+
+        return view;
+    }
+
+    private void initialUserlist() {
+        userlist = new ArrayList<>();
+       //listAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.); //.support_simple_spinner_dropdown_item,userlist);
+        //userlist = new ArrayList<>();
+        //System.out.println("init只會有一次吧");
+    }
+
+    class fetchData extends Thread{
+        String data = "";
+
+        @Override
+        public void run()
+        {
+           // System.out.println("現在跑到這裡沒問題");
+            URL url;
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+
+                    progressDialog = new ProgressDialog(getActivity());
+                    progressDialog.setMessage("Fetching");
+                    progressDialog.show();
+
+                }
+            });
+
+            {
+                try {
+                    System.out.println("第一個要看到");
+                    ParseQuery<ParseObject> query = new ParseQuery<>("Shop");
+                    query.findInBackground(new FindCallback<ParseObject>() {
+                        public void done(List<ParseObject> objects, ParseException e) {
+                            if (e == null) {
+                                                      /* System.out.println("object是什"+objects);
+                                                       String str="";
+                                                       str+=objects.toString();
+                                                       System.out.println("看這裡"+str);*/
+                                // userlist.clear();
+                                for(int k=0;k<10;k++)
+                                {
+                                    ParseObject parseObject = objects.get(k); // Get the object
+                                    parseObject.fetchIfNeededInBackground(new GetCallback<ParseObject>() {
+                                        public void done(ParseObject result, ParseException e) {
+                                            if (e == null)
+                                            {
+                                                System.out.println("看看這result是什麼"+result.getString("shopName"));
+                                                String shopName = result.getString("shopName");
+                                                userlist.add(shopName);
+                                                System.out.println("這裡的userlist呢"+userlist);
+                                            }
+
+                                            // Do something with result
+                                        }
+                                    });
+
+
+                                }
+
+
+                            }
+                            else {
+                                //objectRetrievalFailed();
+                                System.out.println("GGGGGGG");
+                            }
+                        }
+                    });
+
+                   /*  這是只找一個的方式
+                        query.getFirstInBackground(new GetCallback<ParseObject>() {
+                            @Override
+                            public void done(ParseObject object, ParseException e) {
+                                if(e==null)
+                                {
+                                    String str="";
+                                    str+=object.getString("shopName");
+                                    System.out.println("看這裡"+str);
+                                    //object.getJSONObject("")
+                                }
+                                else
+                                {
+                                    System.out.println("error");
+                                }
+                            }
+                        });*/
+
+
+                    url = new URL("http://10.0.2.2:5000/api/Books");//http://10.0.2.2:5000/api/Books
+                    HttpURLConnection httpsURLConnection = (HttpURLConnection) url.openConnection();
+                    InputStream inputStream = httpsURLConnection.getInputStream();
+                    //System.out.println("Input是什麼"+inputStream);
+
+                    //
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    String line;
+                    //System.out.println("line是空的？");
+
+                    //如果buffReader沒有讀到空的就繼續
+                   while ((line = bufferedReader.readLine())!=null)
+                    {
+                        System.out.println("有道line"+data);
+                        data = data+line;
+                        System.out.println("data"+data);
+                    }
+                    //如果data不是空的，就把data抓進Json放到userlist
+
+                    if(!data.isEmpty())
+                    {
+                     //   System.out.println("");
+                       //JSONObject jsonObject = new JSONObject(data);
+                        JSONArray jsonArray = new JSONArray(data);
+                        userlist.clear();
+                        for(int j  = 0;j<jsonArray.length();j++)//10上線需要改
+                        {
+                            JSONObject jsonobject = jsonArray.getJSONObject(j);
+                            String name = jsonobject.getString("name");
+                            System.out.println("這裡要看到name"+name);
+                            userlist.add(name);
+                            System.out.println("這裡的userlist"+userlist);
+
+                        }
+                        //等等資料近來這裡
+
+
+                    }
+
+                    else
+                        System.out.println("到這裡就ＧＧ了");
+
+
+
+
+
+
+                }
+
+
+                catch (MalformedURLException e) {
+
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(progressDialog.isShowing())
+                        {
+                            progressDialog.dismiss();
+                          //  System.out.println("看得到嗎嗎嗎？"+listAdapter);
+                            listAdapter.notifyDataSetChanged();
+                         //   System.out.println("看得到？"+listAdapter);
+                        }
+
+                    }
+                });
+            }
+        }
     }
 
 
 }
+//https://stackoverflow.com/questions/3132021/android-listview-illegalstateexception-the-content-of-the-adapter-has-changed

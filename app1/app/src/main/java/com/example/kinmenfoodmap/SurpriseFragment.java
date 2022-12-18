0c008ab289a376915b54ab99714c2a13fedf2176
Bojun6667 <s111010520@student.nqu.edu.tw>
2022-12-18 17:44:59 +0800

    supbox bojun 12/18

--- app1/app/src/main/java/com/example/kinmenfoodmap/SurpriseFragment.java
+++ app1/app/src/main/java/com/example/kinmenfoodmap/SurpriseFragment.java
@@ -1,6 +1,6 @@
 package com.example.kinmenfoodmap;
 
-import android.location.Location;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,10 +9,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.parse.GetCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.text.BreakIterator;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,13 +36,13 @@ public class SurpriseFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private TextView output;
 
     private double distance = 0.0;
     private double Latitude1 = 25.298218036200446;
     private double Longitude1 = 121.56814583830808;
     private double Latitude2 = 21.905662893203562;
     private double Longitude2 = 120.85092391581581;
-
 
     public SurpriseFragment() {
         // Required empty public constructor
@@ -65,21 +72,59 @@ public class SurpriseFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_surprise, container, false);
 
-        ImageView img_btn = (ImageView) view.findViewById(R.id.supbox);
-
-        img_btn.setOnClickListener(new View.OnClickListener() {
+        ImageView supbox_btn = (ImageView) view.findViewById(R.id.supbox);
+        output = (TextView) view.findViewById(R.id.textView20);
+        supbox_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                System.out.println("btn ok");
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Shop");
+                query.whereEqualTo("shopName", "金食堂");
+
+                System.out.println(query);
+                query.getFirstInBackground(new GetCallback<ParseObject>() {
+                    public void done(ParseObject player, ParseException e) {
+                        if (e == null) {
+                            System.out.println("ok:");
+                            String response="";// lat = "", lng = "";
+                            response += player.getString("shopName");
+                            response += "\n";
+                            response += player.getString("ID");
+                            response += "\n";
+                            response += player.getString("address");
+                            response += "\n";
+                            response += player.getList("menu");
+                            response += "\n";
+                            response += player.getString("closing");
+                            response += "\n";
+                            response += player.getString("business");
+                            response += "\n";
+                            response += player.getParseGeoPoint("latitude_longitude");
+                            response += "\n";
+
+                            //lat += player.getParseGeoPoint("latitude_longitude").getLatitude();
+                            //lng += player.getParseGeoPoint("latitude_longitude").getLongitude();
+                            //latlng += player.getParseGeoPoint("latitude_longitude");
+
+                            System.out.println(response);
+                            output.setText(response);
+                        } else {
+                            System.out.println("error");
+                        }
+
+                    }
+                });
+                //經緯座標距離運算
                 double theta = Longitude1 - Longitude2;
                 double distance = 60 * 1.1515 * (180/Math.PI) * Math.acos(
                         Math.sin(Latitude1 * (Math.PI/180)) * Math.sin(Latitude2 * (Math.PI/180)) +
@@ -87,16 +132,9 @@ public class SurpriseFragment extends Fragment {
                 );
                 double kilodistance = distance / 0.6214;
 
-                System.out.println("距離" + kilodistance);
+                System.out.println("距離: " + kilodistance);
             }
         });
-
         return view;
     }
-
-    public void onClick(View view){
-        return ;
-    }
-
-
 }
